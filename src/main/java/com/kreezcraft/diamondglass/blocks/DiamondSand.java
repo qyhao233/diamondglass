cf5be5c512557378b509b499b3bb5b99d77e64f8
Kreezxil <kreezxil@gmail.com>
2017-07-16 17:46:07 -0500

    thanks again mmd
    In addition to Shadows_of_Fire that helped me factor the majority of the
    code. I give further thanks to RogueLogix and Face_of_Cat for their
    insight into helping resolve the JSON recipe issues and something to do
    with an item model for my blocks.

--- src/main/java/com/kreezcraft/diamondglass/blocks/DiamondSand.java
+++ src/main/java/com/kreezcraft/diamondglass/blocks/DiamondSand.java
@@ -2,32 +2,34 @@ package com.kreezcraft.diamondglass.blocks;
 
 import java.util.Random;
 
+import com.kreezcraft.diamondglass.DiamondGlass;
+import com.kreezcraft.diamondglass.client.IHasModel;
+import com.kreezcraft.diamondglass.items.InitItems;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSand;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.item.EntityFallingBlock;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class DiamondSand extends BlockSand {
+public class DiamondSand extends BlockStandard {
 
     public static boolean fallInstantly;
     
-    protected String name;
-
 	public DiamondSand(String name) {
-		super();
-		this.name=name;
-		setHardness(3f);
-		setResistance(5f);
+		super(Material.SAND, name);
 	}
-	
-	   /**
+
+   /**
      * Called after the block is set in the Chunk data, but before the Tile Entity is set
      */
     public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
--- src/main/java/com/kreezcraft/diamondglass/recipes/ModRecipes.java
+++ src/main/java/com/kreezcraft/diamondglass/recipes/ModRecipes.java
@@ -6,14 +6,15 @@ import com.kreezcraft.diamondglass.items.InitItems;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModRecipes {
 
 	public static void init() {
 		GameRegistry.addSmelting(InitBlocks.DIAMOND_SAND, new ItemStack(InitBlocks.DIAMOND_GLASS), 10f);
-
 	}
 
 }
